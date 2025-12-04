b2eaf91eaf9684220210f3a2d35ab418959780fb
ebuka1017 <isaacokwuzi@gmail.com>
2025-12-04 20:11:46 +0100

    fonts fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,31 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import { ErrorBoundary } from "@/components/error-boundary";
 import "./globals.css";
 
-const poppins = Poppins({
-  weight: ["300", "400", "500", "600", "700"],
-  subsets: ["latin"],
+const poppins = localFont({
+  src: [
+    { path: "../../public/fonts/Poppins/Poppins-Thin.ttf", weight: "100", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-ThinItalic.ttf", weight: "100", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-ExtraLight.ttf", weight: "200", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-ExtraLightItalic.ttf", weight: "200", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-Light.ttf", weight: "300", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-Italic.ttf", weight: "400", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-MediumItalic.ttf", weight: "500", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-SemiBoldItalic.ttf", weight: "600", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-ExtraBoldItalic.ttf", weight: "800", style: "italic" },
+    { path: "../../public/fonts/Poppins/Poppins-Black.ttf", weight: "900", style: "normal" },
+    { path: "../../public/fonts/Poppins/Poppins-BlackItalic.ttf", weight: "900", style: "italic" },
+  ],
   variable: "--font-poppins",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
